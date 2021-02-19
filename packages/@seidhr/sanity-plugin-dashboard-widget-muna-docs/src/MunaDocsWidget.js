5bc765bcc196a7dda3c25e9e4fd65654182f9d62
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2021-02-19 10:08:42 +0100

    Fix typo

--- packages/@seidhr/sanity-plugin-dashboard-widget-muna-docs/src/MunaDocsWidget.js
+++ packages/@seidhr/sanity-plugin-dashboard-widget-muna-docs/src/MunaDocsWidget.js
@@ -10,12 +10,7 @@ const MunaDocsWidget = (props) => {
       </header>
       <div className={styles.content}>
         <SeidhrLogo style={{display: 'block', margin: 'auto'}} width="8em" height="8em" />
-        <p>
-          Muna or ᛗᚢᚾᚨ means remember in norse. Muna is a schema for Sanity that
-          enables detailed descriptions of cultural heritage objects and knowledge about their
-          contexts. Muna is based on the <a href="https://cidoc-crm.org/" target="_blank">CIDOC-CRM model</a> 
-          and <a href="https://linked.art/" target="_blank">linked.art</a>.
-        </p>
+        <p> Muna or ᛗᚢᚾᚨ means remember in norse. Muna is a schema for Sanity that enables detailed descriptions of cultural heritage objects and knowledge about their contexts. Muna is based on the <a href="https://cidoc-crm.org/" target="_blank">CIDOC-CRM model</a> and <a href="https://linked.art/" target="_blank">linked.art</a>.</p>
         <p>
           <a href="https://muna.xyz/docs/model/introduction" target="_blank">
             Muna documentation
