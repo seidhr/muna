66e2a0939fc5b73cde520737a35df5ee10b89389
tarjelavik <Tarje.Lavik@ub.uib.no>
2021-07-05 16:56:20 +0200

    Update test studio

--- packages/examples/test-studio/src/parts/assetSources.js
+++
@@ -1,5 +0,0 @@
-// import SanitySource from 'part:@sanity/form-builder/input/image/asset-source-default'
-import NBiiif from '@seidhr/sanity-plugin-asset-source-nbiiif'
-import Marcus from '@seidhr/sanity-plugin-asset-source-marcus'
-
-export default [NBiiif, Marcus]
