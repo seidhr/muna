57f2b647708a7e5ece595168f1126a834c31d889
Tarje Lavik <tarje.lavik@gmail.com>
2021-12-13 23:52:42 +0100

    Remove unused visualization experiment

--- packages/@seidhr/sanity-plugin-timespan-input/src/components/preview/index.tsx
+++ packages/@seidhr/sanity-plugin-timespan-input/src/components/preview/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ParentSize from '@visx/responsive/lib/components/ParentSize';
 import TreeNode from './TreeNode';
-import AxisVis from './Axis';
 
 const TimespanWrapper = ({ data }) => {
   return (
     <>
       <ParentSize>{({ width, height }) => <TreeNode width={width} height={height} data={data} />}</ParentSize>
-      {/* <ParentSize>{({ width, height }) => <AxisVis width={width} height={height} data={data} />}</ParentSize> */}
     </>
   )
 }
